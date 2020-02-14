6cee371f5f6440a796519ea801a194430da4241f
Dimitri1205 <57532199+Dimitri1205@users.noreply.github.com>
2020-02-14 16:24:01 +0100

    Algorithm re-write

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
 import java.io.*;
 import java.util.Scanner;
 
@@ -7,45 +9,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
-        int chr = 0;
-        int sps = 0;
-        int lnb = 0;
+        String line;
+        int characters = 0;
+        int spaces = 0;
+        int lines = 0;
         int words = 0;
 
+
         try {
-           FileReader fr = new FileReader(new File("MouseMover.txt"));
-           Scanner sc = new Scanner(new File("MouseMover.txt"));
+           Scanner sc = new Scanner(new File("file.txt"));
 
-           while (sc.hasNext()) {
-               words += 1;
-               sc.next();
+           while (sc.hasNextLine()) {
+
+               System.out.println(line = sc.nextLine());
+               lines += 1;
+               
+               String[] wordsInLine = line.split(" ");
+               if (!line.equals(""))
+               words += wordsInLine.length;
+
+               for (char c: line.toCharArray()) {
+                   if (c == ' ') spaces += 1;
+                   else characters += 1;
+               }
            }
-
-           int c = 0;
-           while ((c = fr.read()) != -1) {
-               char cr = (char) c;
-               System.out.print(cr);
-
-               if (cr == '\n')
-                   lnb += 1;
-               else if (cr == ' ')
-                   sps += 1;
-               else chr +=1;
-           }
-
-            System.out.println();
-            System.out.println("Word Count: " + words);
-            System.out.println("Line Breaks: " + lnb);
-            System.out.println("Spaces: " + sps);
-            System.out.println("Characters: " + chr);
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+        System.out.println("----------------------------------");
+        System.out.println("Word Count: " + words);
+        System.out.println("Lines: " + lines);
+        System.out.println("Spaces: " + spaces);
+        System.out.println("Characters: " + characters);
 
 
     }
